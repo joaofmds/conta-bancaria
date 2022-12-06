522d618a3131c74db47698ba651a0c183af011c7
João Felipe Mendes de Souza <joao.mendes@smarthis.com.br>
2022-12-06 17:28:11 -0300

    Cria a classe ContaCorrente

--- contas.py
+++ contas.py
@@ -1,7 +1,7 @@
 import abc
 
 class Conta(abc.ABC):
-    def __init__(self, agencia, conta, saldo):
+    def __init__(self, agencia, conta, saldo=0):
         self.agencia = agencia
         self.conta = conta
         self.saldo = saldo
@@ -29,8 +29,34 @@ class ContaPoupanca(Conta):
         print('Não foi possível sacar o valor desejado.')
         self.detalhes(f'(SAQUE NEGADO {valor})')
 
+class ContaCorrente(Conta):
+    def __init__(self, agencia, conta, saldo=0, limite=0):
+        super().__init__(agencia, conta, saldo)
+        self.limite = limite
+
+    def sacar(self, valor):
+        valor_pos_saque = self.saldo - valor
+        limite_maximo = -self.limite
+
+        if valor_pos_saque >= limite_maximo:
+            self.saldo -= valor
+            self.detalhes(f'(SAQUE {valor})')
+            return self.saldo
+
+        print('Não foi possível sacar o valor desejado.')
+        print(f'Seu limite é {-self.limite:.2f}')
+        self.detalhes(f'(SAQUE NEGADO {valor})')
+
 if __name__ == '__main__':
-    cp1 = ContaPoupanca(111, 222, 0)
-    cp1.sacar(1)
-    cp1.depositar(1)
-    cp1.sacar(1)
+    # cp1 = ContaPoupanca(111, 222, 0)
+    # cp1.sacar(1)
+    # cp1.depositar(1)
+    # cp1.sacar(1)
+    # print('##')
+    cc1 = ContaCorrente(111, 222, 0, 100)
+    cc1.sacar(1)
+    cc1.depositar(1)
+    cc1.sacar(2)
+    cc1.sacar(98)
+    cc1.sacar(1)
+    print('##')
