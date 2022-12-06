e995093c9dae5da90cf24b5ebaa9884bca91467e
João Felipe Mendes de Souza <joao.mendes@smarthis.com.br>
2022-12-06 17:46:47 -0300

    Adiciona tipagem às classes e métodos

--- contas.py
+++ contas.py
@@ -1,19 +1,20 @@
 import abc
 
 class Conta(abc.ABC):
-    def __init__(self, agencia, conta, saldo=0):
+    def __init__(self, agencia: int, conta: int, saldo: float = 0):
         self.agencia = agencia
         self.conta = conta
         self.saldo = saldo
 
     @abc.abstractmethod
-    def sacar(self, valor): ...
+    def sacar(self, valor: float) -> float: ...
 
-    def depositar(self, valor):
+    def depositar(self, valor: float) -> float:
         self.saldo += valor
         self.detalhes(f'(DEPÓSITO {valor})')
+        return self.saldo
 
-    def detalhes(self, msg=''):
+    def detalhes(self, msg: str = '') -> None:
         print(f'O seu saldo é {self.saldo:.2f} {msg}')
         print('--')
 
@@ -28,13 +29,14 @@ class ContaPoupanca(Conta):
 
         print('Não foi possível sacar o valor desejado.')
         self.detalhes(f'(SAQUE NEGADO {valor})')
+        return self.saldo
 
 class ContaCorrente(Conta):
-    def __init__(self, agencia, conta, saldo=0, limite=0):
+    def __init__(self, agencia: int, conta: int, saldo: float = 0, limite: float = 0):
         super().__init__(agencia, conta, saldo)
         self.limite = limite
 
-    def sacar(self, valor):
+    def sacar(self, valor: float) -> float:
         valor_pos_saque = self.saldo - valor
         limite_maximo = -self.limite
 
@@ -46,6 +48,7 @@ class ContaCorrente(Conta):
         print('Não foi possível sacar o valor desejado.')
         print(f'Seu limite é {-self.limite:.2f}')
         self.detalhes(f'(SAQUE NEGADO {valor})')
+        return self.saldo
 
 if __name__ == '__main__':
     # cp1 = ContaPoupanca(111, 222, 0)
